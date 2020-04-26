ccd2343ecd48f45a9aa80a17a62675a183066323
parinitm <webgility123>
2020-04-26 14:30:06 +0530

    Initial Commit

---
+++ src/main/java/guru/springframework/springboot/MvnSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvnSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springboot/MvnSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
